d01f8727bf982cc1b216c09f3792d178de6d6778
Vinicius Silva <vinicius.caetano12@gmail.com>
2023-08-01 21:18:36 -0300

    projeto console em dotnet

---
+++ PROJETO .NET PORTFÓLIO/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Digite seu nome: ");
+            var nome = Console.ReadLine();
+
+            Console.WriteLine($"Olá, {nome}! Bem-vindo ao mundo C#!");
+        }
+    }
+}
